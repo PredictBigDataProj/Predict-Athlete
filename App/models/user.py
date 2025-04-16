bf303ea1ea378c64789290a483f8f93e93615598
Tyrell-Lewis <tyrell.lewis@my.uwi.edu>
2025-04-16 20:04:04 +0000

    added similarity feature for players when you submit the form, and removed need to log in.

--- App/models/user.py
+++ App/models/user.py
@@ -4,7 +4,7 @@ from App.database import db
 
 #Just testing that commits work
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
