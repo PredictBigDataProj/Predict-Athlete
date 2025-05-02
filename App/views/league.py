916425a735ec566b3bf63be23a40197b3ae74433
Tyrell-Lewis <tyrell.lewis@my.uwi.edu>
2025-05-02 17:01:50 +0000

    starting comparative table

--- App/views/league.py
+++ App/views/league.py
@@ -145,6 +145,7 @@ def get_league_page(league_id, country):
     league_df = df[df['league_name_id'] == league_id]
     
     group_counts = league_df['age_group'].value_counts(normalize=True) * 100  # as percentage
+    global_group_counts = df['age_group'].value_counts(normalize=True) * 100  # as percentage
 
     avg_age = league_df['age'].mean()
     min_age = league_df['age'].min()
@@ -154,14 +155,27 @@ def get_league_page(league_id, country):
     avg_career = league_df['league_Career_length'].mean()
     age_counts = league_df['age'].value_counts()
     
+    global_avg_age = df['age'].mean()
+    global_min_age = df['age'].min()
+    global_max_age = df['age'].max()
+    global_max_career = df['league_Career_length'].max()
+    global_min_career = df['league_Career_length'].min()
+    global_avg_career = df['league_Career_length'].mean()
+    global_age_counts = df['age'].value_counts()
 
 
     position_stats = []
+    global_position_stats = []
 
     for pos in positions:
         pos_df = league_df[league_df[pos] == 1]
+        global_pos_df = df[df[pos] == 1]
+
         group_counts_pos = pos_df['age_group'].value_counts(normalize=True) * 100  # as percentage
         age_total_count = pos_df.shape[0]
+
+        global_group_counts_pos = global_pos_df['age_group'].value_counts(normalize=True) * 100  # as percentage
+        global_age_total_count = global_pos_df.shape[0]
 
         if pos_df.empty:
             print(f'No players in this position: {pos}')
@@ -181,23 +195,21 @@ def get_league_page(league_id, country):
             }
             position_stats.append(pos_data)
 
+            global_pos_data = {
+            'position': pos,
+            'avg_age': global_pos_df['age'].mean(),
+            'min_age': global_pos_df['age'].min(),
+            'max_age': global_pos_df['age'].max(),
+            'avg_career': global_pos_df['league_Career_length'].mean(),
+            'min_career': global_pos_df['league_Career_length'].min(),
+            'max_career': global_pos_df['league_Career_length'].max(),
+            'pos_age_groups': global_group_counts_pos.to_dict(),
+            'age_total_count': global_age_total_count
+            }
+            global_position_stats.append(global_pos_data)
 
-    # print("\n")
-    # print(f"Overall for the {league_id} League")
-    # print(f'Average age is : {avg_age:.2f} years old')
-    # print(f'Youngest age is : {min_age:.2f} years old')
-    # print(f'Oldest age is : {max_age:.2f} years old')
-    # print(f'Max Career length is : {max_career:.2f} years')
-    # print(f'Min Career length is : {min_career:.2f} years')
-    # print(f'Average Career length is : {avg_career:.2f} years')
-    # print(f"Age Group Distribution for League: {league_id}")
-    # print(group_counts)
-    # print("\n")
-    # print("=======================================================================")
 
-
-    avg_age = df[df['league_name_id'] == league_id]['age'].mean() #This is the same analysis as what is in the notebook, just use the df transformations in here and send the data as information tot he template.
-
+    
     #END OF AGE SECTION==============================================================================================================
 
     #NATION/COUNTRY SECTION==========================================================================================================
@@ -213,17 +225,30 @@ def get_league_page(league_id, country):
     unique_nations = nation_df['nation_Nation'].unique().tolist()
     unique_regions = nation_df['nation_region'].unique().tolist()
 
+    global_unique_nations = df['nation_Nation'].unique().tolist()
+    global_unique_regions = df['nation_region'].unique().tolist()
+
     total_count = nation_df.shape[0]#
     nation_count = nation_df['nation_Nation'].value_counts()
     region_count = nation_df['nation_region'].value_counts()
 
+    global_total_count = df.shape[0]#
+    global_nation_count = df['nation_Nation'].value_counts()
+    global_region_count = df['nation_region'].value_counts()
+
     max_nation_name = nation_count.idxmax()
     max_nation_num = nation_count.max()
+
+    global_max_nation_name = global_nation_count.idxmax()
+    global_max_nation_num = global_nation_count.max()
 
    # region_count = nation_df['nation_region'].value_counts()
 
     max_region_name = region_count.idxmax()
     max_region_num = region_count.max()
+
+    global_max_region_name = global_region_count.idxmax()
+    global_max_region_num = global_region_count.max()
     
 
     
@@ -235,15 +260,28 @@ def get_league_page(league_id, country):
         pos: {"region_count": 0, "region": None} for pos in positions
     }
 
+
+    global_nation_max_by_position = {
+        pos: {"count": 0, "nation": None} for pos in positions
+    }
+
+    global_region_max_by_position = {
+        pos: {"region_count": 0, "region": None} for pos in positions
+    }
+
     distribution = []
     region_distribution = []
+
+    global_distribution = []
+    global_region_distribution = []
 
     for nation in unique_nations:
         spec_count = nation_df[nation_df['nation_Nation'] == nation].shape[0]
         percentage_count = (spec_count / total_count) * 100
-        # print("\n")
-        # print(f'The distribution of players for {nation} is: {percentage_count:.2f}%')
-        # print(f'The number of players in {nation} is: {spec_count}')
+
+        global_spec_count = df[df['nation_Nation'] == nation].shape[0]
+        global_percentage_count = (global_spec_count / global_total_count) * 100
+     
 
         nation_data = {
             "nation": nation,
@@ -252,11 +290,22 @@ def get_league_page(league_id, country):
             "positions": {}
         }
 
+        global_nation_data = {
+            "nation": nation,
+            "percentage": round(global_percentage_count, 2),
+            "count": global_spec_count,
+            "positions": {}
+        }
+
         for pos in positions:
             pos_df = nation_df[(nation_df[pos] == 1) & (nation_df['nation_Nation'] == nation)]
             count_pos = pos_df.shape[0]
 
+            global_pos_df = df[(df[pos] == 1) & (df['nation_Nation'] == nation)]
+            global_count_pos = global_pos_df.shape[0]
+
             nation_data["positions"][pos] = count_pos
+            global_nation_data["positions"][pos] = global_count_pos
 
             # print(f'Number of players that are {pos} is :{count_pos} from {nation}')
 
@@ -269,7 +318,17 @@ def get_league_page(league_id, country):
                     "nation": nation
                 }
 
+            if global_count_pos > global_nation_max_by_position[pos]["count"]:
+                global_nation_max_by_position[pos]["count"] = global_count_pos
+                global_nation_max_by_position[pos]["nation"] = nation
+
+                global_nation_max_by_position[pos] = {
+                    "count": global_count_pos,
+                    "nation": nation
+                }
+
         distribution.append(nation_data)
+        global_distribution.append(global_nation_data)
 
 
     nation_results = {
@@ -281,13 +340,23 @@ def get_league_page(league_id, country):
         "max_by_position": nation_max_by_position
     }
 
+    global_nation_results = {
+        "total_players": global_total_count,
+        "most_common_nation": global_max_nation_name,
+        "most_common_nation_count": global_max_nation_num,
+        "unique_nations": global_unique_nations,
+        "distribution": global_distribution,
+        "max_by_position": global_nation_max_by_position
+    }
+
 
     for region in unique_regions:
         region_spec_count = nation_df[nation_df['nation_region'] == region].shape[0]
         region_percentage_count = (region_spec_count / total_count) * 100
-        # print("\n")
-        # print(f'The distribution of players for {nation} is: {percentage_count:.2f}%')
-        # print(f'The number of players in {nation} is: {spec_count}')
+
+        global_region_spec_count = df[df['nation_region'] == region].shape[0]
+        global_region_percentage_count = (global_region_spec_count / global_total_count) * 100
+   
 
         region_data = {
             "region": region,
@@ -296,11 +365,22 @@ def get_league_page(league_id, country):
             "positions": {}
         }
 
+        global_region_data = {
+            "region": region,
+            "region_percentage": round(global_region_percentage_count, 2),
+            "region_count": global_region_spec_count,
+            "positions": {}
+        }
+
         for pos in positions:
             pos_df = nation_df[(nation_df[pos] == 1) & (nation_df['nation_region'] == region)]
             region_count_pos = pos_df.shape[0]
 
+            global_pos_df = df[(df[pos] == 1) & (df['nation_region'] == region)]
+            global_region_count_pos = global_pos_df.shape[0]
+
             region_data["positions"][pos] = region_count_pos
+            global_region_data["positions"][pos] = global_region_count_pos
 
             # print(f'Number of players that are {pos} is :{count_pos} from {nation}')
 
@@ -313,7 +393,18 @@ def get_league_page(league_id, country):
                     "region": region
                 }
 
+            if global_region_count_pos > global_region_max_by_position[pos]["region_count"]:
+                global_region_max_by_position[pos]["region_count"] = global_region_count_pos
+                global_region_max_by_position[pos]["region"] = region
+
+                global_region_max_by_position[pos] = {
+                    "region_count": global_region_count_pos,
+                    "region": region
+                }
+
         region_distribution.append(region_data)
+        global_region_distribution.append(global_region_data)
+
 
 
     region_results = {
@@ -323,6 +414,15 @@ def get_league_page(league_id, country):
         "unique_regions": unique_regions,
         "region_distribution": region_distribution,
         "region_max_by_position": region_max_by_position
+    }
+
+    global_region_results = {
+        "region_total_players": global_total_count,
+        "most_common_region": global_max_region_name,
+        "most_common_region_count": global_max_region_num,
+        "unique_regions": global_unique_regions,
+        "region_distribution": global_region_distribution,
+        "region_max_by_position": global_region_max_by_position
     }
 
 
@@ -336,32 +436,43 @@ def get_league_page(league_id, country):
     attr_max = {}
     attr_min = {}
     attr_avg = {}
+
+
+    global_attr_max = {}
+    global_attr_min = {}
+    global_attr_avg = {}
     
     
     
     
     left_df = final_df[final_df['preferred_foot_Left'] == 1]
     right_df = final_df[final_df['preferred_foot_Right'] == 1]
+
+    global_left_df = df[df['preferred_foot_Left'] == 1]
+    global_right_df = df[df['preferred_foot_Right'] == 1]
     
     count_left_total = left_df.shape[0]
     count_right_total = right_df.shape[0]
+
+    global_count_left_total = global_left_df.shape[0]
+    global_count_right_total = global_right_df.shape[0]
     
     total_ovr = count_left_total + count_right_total
-    
-    # print(f'Total Number of players in {league_id} are: {total_ovr}')
-    # print(f'Total Number of left footed players {league_id} are: {count_left_total}')
-    # print(f'Total Number of right footed players  {league_id} are: {count_right_total}')
+    global_total_ovr = global_count_left_total + global_count_right_total
+
     
     attribute_data = []
+    global_attribute_data = []
 
     for attr in player_attributes_1:
         total_attr_max = final_df[attr].max()
         total_attr_min = final_df[attr].min()
         total_attr_avg = final_df[attr].mean()
-        # print (f'The Overall maximum {attr} is: {total_attr_max}')
-        # print (f'The Overall minimum {attr} is: {total_attr_min}')
-        # print (f'The Overall average {attr} is: {total_attr_avg}')
-        # print("\n")
+
+        global_total_attr_max = df[attr].max()
+        global_total_attr_min = df[attr].min()
+        global_total_attr_avg = df[attr].mean()
+
 
         attri_data = {
             'attribute': attr,
@@ -371,20 +482,37 @@ def get_league_page(league_id, country):
             }
         attribute_data.append(attri_data)
 
+        global_attri_data = {
+            'attribute': attr,
+            'avg_score': df[attr].mean(),
+            'min_score': df[attr].min(),
+            'max_score': df[attr].max(),
+            }
+        global_attribute_data.append(global_attri_data)
+
         
 
 
     pos_attribute_data = []
+    global_pos_attribute_data = []
     
     for pos in positions:
         pos_df = final_df[final_df[pos] == 1]
+        global_pos_df = df[df[pos] == 1]
 
         left_df = pos_df[pos_df['preferred_foot_Left'] == 1]
         right_df = pos_df[pos_df['preferred_foot_Right'] == 1]
 
+        global_left_df = global_pos_df[global_pos_df['preferred_foot_Left'] == 1]
+        global_right_df = global_pos_df[global_pos_df['preferred_foot_Right'] == 1]
+
         count_left = left_df.shape[0]
         count_right = right_df.shape[0]
         pos_total = count_left + count_right
+
+        global_count_left = global_left_df.shape[0]
+        global_count_right = global_right_df.shape[0]
+        global_pos_total = global_count_left + global_count_right
 
         pos_attri_data = {
             'position': pos,
@@ -394,6 +522,13 @@ def get_league_page(league_id, country):
             "attributes": {}
             }
         
+        global_pos_attri_data = {
+            'position': pos,
+            'count_left': global_count_left,
+            'count_right': global_count_right,
+            'pos_total': global_pos_total,
+            "attributes": {}
+            }
         
         if abs(count_left - count_right) < 5 or abs(count_left - count_right) < (0.10 * pos_total):
             print(f'Not really a better chance if you are left footed or right footed as a {pos} in {league_id}')
@@ -408,20 +543,15 @@ def get_league_page(league_id, country):
                 #foot_score = 1 if player_foot == 'Right' else 0.5
     
 
-
-
-
-        # print("\n")
-
-
-        
-        # print(f'Number of left footed players that are {pos}: {count_left}')
-        # print(f'Number of right footed players that are {pos}: {count_right}')
         
         if pos not in attr_max:
             attr_max[pos] = {}
             attr_min[pos] = {}
             attr_avg[pos] = {}
+
+            global_attr_max[pos] = {}
+            global_attr_min[pos] = {}
+            global_attr_avg[pos] = {}
         for attr in player_attributes_1:
             attr_max[pos][attr] = pos_df[attr].max()
             attr_min[pos][attr] = pos_df[attr].min()
@@ -431,23 +561,41 @@ def get_league_page(league_id, country):
             pos_attr_min = attr_min[pos][attr]
             pos_attr_avg = attr_avg[pos][attr]
 
-            # print (f'The maximum {attr} for {pos} is: {attr_max[pos][attr]}')
-            # print (f'The minimum {attr} for {pos} is: {attr_min[pos][attr]}')
-            # print (f'The average {attr} for {pos} is: {attr_avg[pos][attr]}')
-            # print("\n")
+            global_attr_max[pos][attr] = global_pos_df[attr].max()
+            global_attr_min[pos][attr] = global_pos_df[attr].min()
+            global_attr_avg[pos][attr] = global_pos_df[attr].mean()
+
+            global_pos_attr_max = global_attr_max[pos][attr]
+            global_pos_attr_min = global_attr_min[pos][attr]
+            global_pos_attr_avg = global_attr_avg[pos][attr]
+
+
 
             pos_attri_data["attributes"][attr] = {
                 'pos_attr_max': pos_attr_max,
                 'pos_attr_min': pos_attr_min,
                 'pos_attr_avg': pos_attr_avg,
             }
+
+            global_pos_attri_data["attributes"][attr] = {
+                'pos_attr_max': global_pos_attr_max,
+                'pos_attr_min': global_pos_attr_min,
+                'pos_attr_avg': global_pos_attr_avg,
+            }
         pos_attribute_data.append(pos_attri_data)
+        global_pos_attribute_data.append(global_pos_attri_data)
             
     
     # print("\n")
 
      # Replace NaN values with 0 or a placeholder if no players exist for the position
     for position in pos_attribute_data:
+        if position['pos_total'] == 0:  # No players for this position
+            for attr, values in position['attributes'].items():
+                for key in ['pos_attr_max', 'pos_attr_min', 'pos_attr_avg']:
+                    values[key] = 0  # Replace NaN with 0 or another default value like "No Data"
+
+    for position in global_pos_attribute_data:
         if position['pos_total'] == 0:  # No players for this position
             for attr, values in position['attributes'].items():
                 for key in ['pos_attr_max', 'pos_attr_min', 'pos_attr_avg']:
@@ -469,14 +617,29 @@ def get_league_page(league_id, country):
 
     return render_template('league.html', league_id=league_id, country=country, league_info=league_info.to_dict(orient='records'),
                             avg_age = avg_age, min_age = min_age, max_age = max_age,
+                            global_avg_age = global_avg_age, global_min_age = global_min_age, global_max_age = global_max_age,
+
                             avg_career = avg_career, min_career = min_career, max_career = max_career,
+                            global_avg_career = global_avg_career, global_min_career = global_min_career, global_max_career = global_max_career,
+                            
                             position_stats=position_stats,
+                            global_position_stats=global_position_stats,
+
                             age_groups=group_counts.to_dict(),
+                            global_age_groups=global_group_counts.to_dict(),
+
                             age_counts = age_counts.to_dict(),
+                            global_age_counts = global_age_counts.to_dict(),
+
                             nav_name=nav_name,
                             nation_results=nation_results, region_results=region_results,
+                            global_nation_results=global_nation_results, global_region_results=global_region_results,
+
                             left_footed_players=count_left_total, right_footed_players=count_right_total, total_players=total_ovr,
-                            attribute_data= attribute_data, pos_attribute_data= pos_attribute_data
+                            global_left_footed_players=global_count_left_total, global_right_footed_players=global_count_right_total, global_total_players=global_total_ovr,
+
+                            attribute_data= attribute_data, pos_attribute_data= pos_attribute_data,
+                            global_attribute_data= global_attribute_data, global_pos_attribute_data= global_pos_attribute_data
                              )
 
 
