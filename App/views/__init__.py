1f4ffea917a72336cbe98bd337481ff5481ee724
Isa Abdul-Hamid <isaabdulhamid13@gmail.com>
2025-05-09 08:30:42 -0400

    Started removing templates Removed auth_views from init.py Removed auth.py in App\views

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -2,10 +2,9 @@
 # explicitly instead of using *
 from .user import user_views
 from .index import index_views
-from .auth import auth_views
 from .admin import setup_admin
 from .league import league_views
 
 
-views = [user_views, index_views, auth_views, league_views] 
+views = [user_views, index_views, league_views] 
 # blueprints must be added to this list
